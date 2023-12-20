df2a44bf5a6be9262f93061a6e9cddd745a50cd4
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2023-12-20 11:14:30 +0100

    fix

--- src/Game.java
+++ src/Game.java
@@ -6,11 +6,11 @@ public class Game {
 
   private Pitch pitch;
   private boolean gameOver = false;
-  private boolean valid = true;
 
   // functions
 
   public void printBoard() {
+    int s = 0;
     Field[] fields = this.pitch.getFields();
     int[][] output = new int[(int) Math.sqrt(fields.length)][(int) Math.sqrt(
       fields.length
@@ -20,45 +20,87 @@ public class Game {
       output[field.getX()][field.getY()] = field.getValue();
     }
 
+    while (s < (output.length * 2 + 3)) {
+      System.out.print("-");
+      s++;
+    }
+    System.out.println();
+
     for (int i = 0; i < output.length; i++) {
+      System.out.print("| ");
       for (int j = 0; j < output[i].length; j++) {
         System.out.print(output[i][j] + " ");
       }
+      System.out.print("|");
       System.out.println();
     }
+
+    s = 0;
+    while (s < (output.length * 2 + 3)) {
+      System.out.print("-");
+      s++;
+    }
+    System.out.println();
   }
 
   public void initBoardRandom() {
     this.pitch = new Pitch();
-    this.pitch.init(9);
+    this.pitch.init(3);
   }
 
   public boolean isGameOver() {
     return this.gameOver;
   }
 
-  public void isValid() {
+  public void tryAgain() {
     Scanner scan = new Scanner(System.in);
-    do {
-      System.out.println("move number: ");
-      int number = scan.nextInt();
-      valid = pitch.isValidSelection(number);
-    } while (valid == false);
+    System.out.println("Want to try again? (Y/N)");
+    String again = scan.next();
+    scan.close();
+    if (again == "Y" || again == "y") {} else {
+      System.out.println("Good bye");
+    }
+  }
+
+  public boolean isValid() {
+    Scanner scan = new Scanner(System.in);
+    boolean valid;
+
+    System.out.println("move number: ");
+    int number = scan.nextInt();
+
+    scan.close();
+    valid = this.pitch.isValidSelection(number);
 
     if (valid == false) {
       System.out.println("number cant be moved");
     }
 
-    if (this.isGameOver()) {
-      scan.close();
-    }
+    return valid;
   }
 
-  public static void main(String[] args) {
+  public void input() {
+    boolean valid;
+    do {
+      valid = this.isValid();
+    } while (valid == false);
+  }
+
+  public void main(String[] args) {
     Game game = new Game();
     game.initBoardRandom();
+    int counter = 0;
 
-    System.out.println("Welcome to switch game");
-    game.printBoard();
+    System.out.println("Welcome to 'Switch Game'");
+    System.out.println();
+
+    while (this.isGameOver() == false) {
+      game.printBoard();
+      this.input();
+      counter++;
+    }
+
+    System.out.println("Great, you did it");
+    System.out.println("You needed " + counter + " moves");
   }
 }
