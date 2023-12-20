b0ae9aab96c1b169d50eb544cea6b79001835bd5
Aliena12 <aliena.knecht03@gmail.com>
2023-12-20 11:05:45 +0100

    ja5

--- src/Game.java
+++ src/Game.java
@@ -10,6 +10,7 @@ public class Game {
   // functions
 
   public void printBoard() {
+    int s = 0;
     Field[] fields = this.pitch.getFields();
     int[][] output = new int[(int) Math.sqrt(fields.length)][(int) Math.sqrt(
       fields.length
@@ -19,12 +20,27 @@ public class Game {
       output[field.getX()][field.getY()] = field.getValue();
     }
 
+    while(s < (output.length*2+3)){
+      System.out.print("-");
+      s++;
+    }
+    System.out.println();
+
     for (int i = 0; i < output.length; i++) {
+      System.out.print("| ");
       for (int j = 0; j < output[i].length; j++) {
         System.out.print(output[i][j] + " ");
       }
+      System.out.print("|");
       System.out.println();
     }
+
+    s = 0;
+    while(s < (output.length*2+3)){
+      System.out.print("-");
+      s++;
+    }
+    System.out.println();
   }
 
   public void initBoardRandom() {
@@ -36,6 +52,18 @@ public class Game {
     return this.gameOver;
   }
 
+  public void tryAgain(){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Want to try again? (Y/N)");
+    String again = scan.next();
+    if( again == "Y" || again == "y"){
+
+    }
+    else{
+      System.out.println("Good bye");
+    }
+  }
+
   public boolean isValid() {
     Scanner scan = new Scanner(System.in);
     boolean valid;
@@ -44,7 +72,7 @@ public class Game {
     int number = scan.nextInt();
 
     scan.close();
-    valid = pitch.isValidSelection(number);
+    valid = this.pitch.isValidSelection(number);
 
     if (valid == false) {
       System.out.println("number cant be moved");
@@ -56,15 +84,26 @@ public class Game {
   public void input() {
     boolean valid;
     do {
-      valid = isValid();
+      valid = this.isValid();
     } while (valid == false);
   }
 
-  public static void main(String[] args) {
+  public void main(String[] args) {
     Game game = new Game();
     game.initBoardRandom();
+    int counter = 0;
 
-    System.out.println("Welcome to switch game");
-    game.printBoard();
+    System.out.println("Welcome to 'Switch Game'");
+    System.out.println();
+
+    while(this.isGameOver()==false){
+      game.printBoard();
+      this.input();
+      counter++;
+    }
+
+    System.out.println("Great, you did it");
+    System.out.println("You needed " + counter + " moves");
+ 
   }
 }
