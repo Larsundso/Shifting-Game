ddb4037154ee4d7927f14de2deacceb9fce7cf5a
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2023-12-20 11:11:44 +0100

    fix

---
+++ src/Game.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Game {
+
+  // values
+
+  private Pitch pitch;
+  private boolean gameOver = false;
+  private boolean valid = true;
+
+  // functions
+
+  public void printBoard() {
+    Field[] fields = this.pitch.getFields();
+    int[][] output = new int[(int) Math.sqrt(fields.length)][(int) Math.sqrt(
+      fields.length
+    )];
+
+    for (Field field : fields) {
+      output[field.getX()][field.getY()] = field.getValue();
+    }
+
+    for (int i = 0; i < output.length; i++) {
+      for (int j = 0; j < output[i].length; j++) {
+        System.out.print(output[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public void initBoardRandom() {
+    this.pitch = new Pitch();
+    this.pitch.init(9);
+  }
+
+  public boolean isGameOver() {
+    return this.gameOver;
+  }
+
+  public void isValid() {
+    Scanner scan = new Scanner(System.in);
+    do {
+      System.out.println("move number: ");
+      int number = scan.nextInt();
+      valid = pitch.isValidSelection(number);
+    } while (valid == false);
+
+    if (valid == false) {
+      System.out.println("number cant be moved");
+    }
+
+    if (this.isGameOver()) {
+      scan.close();
+    }
+  }
+
+  public static void main(String[] args) {
+    Game game = new Game();
+    game.initBoardRandom();
+
+    System.out.println("Welcome to switch game");
+    game.printBoard();
+  }
+}
